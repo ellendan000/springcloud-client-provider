4dad25964c44506d80509743785c537a9548d026
Ellen Dan <ellendan000@gmail.com>
2018-05-15 22:50:47 +0800

    add hello controller

---
+++ src/main/java/net/shadow/springcloud/HelloController.java
@@ -0,0 +1,24 @@
+package net.shadow.springcloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.MessageFormat;
+import java.util.logging.Logger;
+
+@RestController
+public class HelloController {
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
+    @Autowired
+    private Registration registration;
+
+    @GetMapping("/hello")
+    public String index(){
+        logger.info(MessageFormat.format("/hello, host: {}, service_id: {}",
+                registration.getHost(), registration.getServiceId()));
+        return "hello cloud";
+    }
+}
