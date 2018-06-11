b246f40c691da7fa25f09d10943bbe0c377da1cc
rldan <rldan@thoughtworks.com>
2018-06-11 16:17:50 +0800

    add config server

--- src/main/java/net/shadow/springcloud/HelloController.java
+++ src/main/java/net/shadow/springcloud/HelloController.java
@@ -1,6 +1,7 @@
 package net.shadow.springcloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,9 @@ import java.util.logging.Logger;
 public class HelloController {
     private final Logger logger = Logger.getLogger(getClass().getName());
 
+    @Value("${from}")
+    private String from;
+
     @Autowired
     private Registration registration;
 
@@ -20,5 +24,10 @@ public class HelloController {
         logger.info(MessageFormat.format("/hello, host: {}, service_id: {}",
                 registration.getHost(), registration.getServiceId()));
         return "hello cloud";
+    }
+
+    @GetMapping("/from")
+    public String from(){
+        return this.from;
     }
 }
